f98255402b51bd9b1b82333ea64859c601100185
daveyk00 <github@daveyk.com>
2025-06-11 09:29:40 +1000

    Added blocking flash to javascript for adobe dc

--- Windows-DisableJavascriptOnAdobeDC.ps1
+++ Windows-DisableJavascriptOnAdobeDC.ps1
@@ -1,2 +1,3 @@
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\") -ne $true) {  New-Item "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\' -Name 'bDisableJavaScript' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\' -Name 'bEnableFlash' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
