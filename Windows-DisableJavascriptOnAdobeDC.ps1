98eb6db628fe07a4186234fe83a7715714ad2c7f
daveyk00 <github@daveyk.com>
2025-11-06 08:56:55 +1000

    Added adobe reader to Windows-DisableJavascriptOnAdobeDC.ps1

--- Windows-DisableJavascriptOnAdobeDC.ps1
+++ Windows-DisableJavascriptOnAdobeDC.ps1
@@ -1,3 +1,8 @@
 if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\") -ne $true) {  New-Item "HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\" -force -ea SilentlyContinue };
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\' -Name 'bDisableJavaScript' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
 New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\' -Name 'bEnableFlash' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+
+
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Adobe Reader\DC\FeatureLockDown\") -ne $true) {  New-Item "HKLM:\SOFTWARE\Policies\Adobe\Adobe Reader\DC\FeatureLockDown\" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Reader\DC\FeatureLockDown\' -Name 'bDisableJavaScript' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Adobe Reader\DC\FeatureLockDown\' -Name 'bEnableFlash' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
