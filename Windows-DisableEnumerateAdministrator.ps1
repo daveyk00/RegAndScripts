0b7c765dad298aca6f54e3c4a6fd2da495a3cfd5
Dave <dave@daveyk.com>
2025-11-28 08:00:46 +1000

    Fixed error in enumerate administrators

--- Windows-DisableEnumerateAdministrator.ps1
+++ Windows-DisableEnumerateAdministrator.ps1
@@ -1,2 +1,2 @@
-if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Windows\CurrentVersion\Policies\CredUI") -ne $true) {  New-Item "HKLM:\SOFTWARE\Windows\CurrentVersion\Policies\CredUI" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Windows\CurrentVersion\Policies\CredUI' -Name 'EnumerateAdministrators' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI") -ne $true) {  New-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI' -Name 'EnumerateAdministrators' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
