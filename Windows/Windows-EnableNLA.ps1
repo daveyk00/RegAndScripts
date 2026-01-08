75981ca6b06c56ba593f53e62aec0d59dbb7796f
Dave <dave@daveyk.com>
2026-01-09 09:07:56 +1000

    Modify enable nla

--- Windows/Windows-EnableNLA.ps1
+++ Windows/Windows-EnableNLA.ps1
@@ -1,2 +1,2 @@
-if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations" -force -ea SilentlyContinue };
-New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations' -Name 'UserAuthentication' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
+if((Test-Path -LiteralPath "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp") -ne $true) {  New-Item "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -force -ea SilentlyContinue };
+New-ItemProperty -LiteralPath 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue;
